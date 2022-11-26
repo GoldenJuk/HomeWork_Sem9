771e4c2c781b4f15cda597d9f12216b66805eef1
Alex Kizilov <as_k@list.ru>
2022-11-26 18:00:34 +0400

    Finished HW

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,6 +3,31 @@
 
 Console.Clear();
 
+int GetAckermanFunction(int num1, int num2)
+{
+    if (num1 == 0)
+        return (num2 + 1);
 
+    else
+        if (num1 > 0 && num2 == 0)
+        return GetAckermanFunction(num1 - 1, 1);
+
+    else
+        return GetAckermanFunction(num1 - 1, GetAckermanFunction(num1, num2 - 1));
+}
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if (m > 0 && n > 0)
+{
+    int result = GetAckermanFunction(m, n);
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Введите неотрицательные числа.");
 
 Console.ReadKey();
